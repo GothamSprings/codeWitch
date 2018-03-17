8868120bc6fbd5098ffa34a1fcc0cabf5564e422
Shiau-uen Ding <ssding@yahoo.com>
2018-03-17 10:24:15 -0400

    deleted commented-out lines

--- src/components/Sandbox.js
+++ src/components/Sandbox.js
@@ -88,10 +88,8 @@ class Sandbox extends Component {
 
     return (
       <div>
-        <div>
-          <Stage width={window.innerWidth/2} height={window.innerHeight-100}>
-        // <div style={divStyle}>
-          // <Stage width={this.state.stageWidth} height={this.state.stageHeight}>
+        <div style={divStyle}>
+          <Stage width={this.state.stageWidth} height={this.state.stageHeight}>
             <Layer>
               <Witch y={this.state.witchY} x={this.state.witchX} color={this.state.color}/>
             </Layer>
