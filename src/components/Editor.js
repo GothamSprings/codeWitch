721207c74faf0a3733b13c83ab92b452c3a71d4c
Mimi Zhao <mimi.zhao@berkeley.edu>
2018-03-20 16:28:34 -0400

    She moves AND stops

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,3 +1,4 @@
+/* eslint no-loop-func: 0 */
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
@@ -6,52 +7,75 @@ import AceEditor from 'react-ace'
 import 'brace/mode/javascript'
 import 'brace/theme/tomorrow'
 
-import { dispatchTextChange, dispatchChangeWitchX, dispatchChangeWitchY } from '../store'
+import { dispatchTextChange, dispatchChangeWitchX, dispatchChangeWitchY, dispatchWitchReset } from '../store'
 
 class Editor extends Component {
   constructor(props){
     super(props)
     console.log(props);
     this.state = {
-      textVal: "hey",
-      coords: []
+      annotations: [],
+      markers: []
     }
     this.handleRun = this.handleRun.bind(this)
   }
 
   handleRun(){
     let actionCoordArr = [];
-    // Pull down game state from props.
-    let testCoords = {
-      witchX: this.props.witchX,
-      witchY: this.props.witchY
-    }
 
+    // Gets text editor commands and parses them.
     let actions = this.props.textValue;
     actions = actions.split('\n');
-    console.log(actions)
-    for(let i = 1; i <= actions.length; i++){
-      switch(actions[i-1]){
-        case "witch.moveRight();":
-          //this.props.onAction("X", 50)
-           setTimeout(function(){
-             this.props.onAction("X", 50)
-           }.bind(this), (500*i))
-          break;
-        case "witch.moveLeft();":
-          this.props.onAction("X", -50)
-          break;
-        case "witch.moveDown();":
-          this.props.onAction("Y", 50)
-          break;
-        case "witch.moveUp();":
-          this.props.onAction("Y", -50)
-          break;
-        default:
-          console.log("Wrong Syntax, probably");
-          break;
-      }
 
+    // Resets the witch to the start.
+    this.props.resetWitch();
+
+    let stopSign = false;
+
+    for(let i = 1; i <= actions.length; i++){
+      setTimeout(function () {
+        switch(actions[i-1]){
+          case "witch.moveRight();":
+            if(!stopSign){
+              this.props.onAction("X", 50)
+            }
+            break;
+          case "witch.moveLeft();":
+            if (!stopSign) {
+              this.props.onAction("X", -50)
+            }
+            break;
+          case "witch.moveDown();":
+            if (!stopSign) {
+              this.props.onAction("Y", 50)
+            }
+            break;
+          case "witch.moveUp();":
+            if (!stopSign) {
+              this.props.onAction("Y", -50)
+            }
+            break;
+          case "":
+            break;
+          default:
+            stopSign = true;
+            this.setState({
+              annotations: [...this.state.annotations, {
+                row: (i - 1),
+                text: 'Syntax Error at Line ' + (i) + '. ' + actions[i-1] + " is not a function.",
+                type: 'error'
+              } ],
+              markers: [...this.state.markers, {
+                startRow: (i-1),
+                endRow: i,
+                className: 'error-marker',
+                type: 'background'
+              }]
+            })
+            console.log("Wrong Syntax, probably");
+            break;
+        }
+      }.bind(this), (500 * i))
 
       let currentCoord = {
         witchX: this.props.witchX,
@@ -74,6 +98,8 @@ class Editor extends Component {
           height="300px"
           width="300px"
           focus={true}
+          annotations={this.state.annotations}
+          markers={this.state.markers}
           wrapEnabled={true}
           value={this.props.textValue}
         />
@@ -99,10 +125,13 @@ const mapDispatch = (dispatch) => {
     },
     onAction(direction, units){
       if(direction === "Y"){
-        dispatch(dispatchChangeWitchY(units))
+        dispatch(dispatchChangeWitchY(units));
       } else if (direction ==="X"){
-        dispatch(dispatchChangeWitchX(units))
+        dispatch(dispatchChangeWitchX(units));
       }
+    },
+    resetWitch(){
+      dispatch(dispatchWitchReset());
     }
   }
 }
--- src/store/witchCoords.js
+++ src/store/witchCoords.js
@@ -1,19 +1,23 @@
-const WITCH_X_RIGHT = 'WITCH_X_RIGHT'
-const WITCH_Y_DOWN = 'WITCH_Y_DOWN'
+const WITCH_MOVE_X = 'WITCH_X_RIGHT'
+const WITCH_MOVE_Y = 'WITCH_Y_DOWN'
+const WITCH_RESET_LOCATION = 'WITCH_RESET_LOCATION'
 
-const changeWitchX = (witchX) => ({ type: WITCH_X_RIGHT, witchX });
-const changeWitchY = (witchY) => ({ type: WITCH_Y_DOWN, witchY })
-
+const changeWitchX = (witchX) => ({ type: WITCH_MOVE_X, witchX });
+const changeWitchY = (witchY) => ({ type: WITCH_MOVE_Y, witchY })
+const witchResetLocation = () => ({ type: WITCH_RESET_LOCATION })
 
 export const dispatchChangeWitchX = (witchX) => (dispatch) => dispatch(changeWitchX(witchX));
 export const dispatchChangeWitchY = (witchY) => (dispatch) => dispatch(changeWitchY(witchY));
+export const dispatchWitchReset = () => (dispatch) => dispatch(witchResetLocation());
 
 export default function (state = {witchX: 0, witchY:0}, action) {
   switch(action.type) {
-    case WITCH_X_RIGHT:
+    case WITCH_MOVE_X:
       return Object.assign({}, state, { witchX: state.witchX + action.witchX });
-    case WITCH_Y_DOWN:
+    case WITCH_MOVE_Y:
       return Object.assign({}, state, { witchY: state.witchY + action.witchY });
+    case WITCH_RESET_LOCATION:
+      return Object.assign({}, state, {witchX: 0, witchY: 0});
     default:
       return state;
   }
