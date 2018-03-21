cbbe4d1b2b6cac322e40a0a5ba240aa5c9041a15
Shiau-uen Ding <ssding@yahoo.com>
2018-03-21 13:10:14 -0400

    Blockly arrived

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import Blocks from './blocks/Blocks'
+import '../css/App.css';
+import Sandbox from './Sandbox';
+import Blocks from './blocks/Blocks';
+import Game from './Game';
+import Board from './Board';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="gameContainer">
+          <Sandbox />
           <Blocks />
+          <Game />
+          <Board />
         </div>
       </div>
     );
