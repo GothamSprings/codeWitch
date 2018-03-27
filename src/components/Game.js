49168486dc39e57e6fd64753fa769e9301bf2281
Roxie Turner <roxie.turner@gmail.com>
2018-03-27 15:27:36 -0400

    assets and some lil styling thingies

--- src/components/Game.js
+++ src/components/Game.js
@@ -3,19 +3,27 @@ import { connect } from 'react-redux'
 import {Sandbox, Editor} from './'
 import Blocks from './blocks/Blocks'
 
+const GREY = "#9E9E9E";
+
 const style = {
   display: 'flex',
+  flexWrap: 'wrap',
   justifyContent: 'center',
   margin: 'auto',
-  paddingTop: 75
-}
+  paddingTop: 50,
+};
+
+
+const shadow = {
+  boxShadow: `0px 0px 25px 10px ${GREY}`
+};
 
 class Game extends Component {
   constructor (props) {
     super(props);
-    
+
   }
-  
+
   componentDidMount () {
     this.setState({
       type: this.state
@@ -27,15 +35,17 @@ class Game extends Component {
     if (this.props.gameType === 'blockly') {
       return (
         <div style={style}>
-          <Sandbox level={this.props.match.params.id}/>
-          <Blocks level={this.props.match.params.id}/>
+            <Sandbox level={this.props.match.params.id}/>
+            <Blocks level={this.props.match.params.id}/>
         </div>
       )
     } else if (this.props.gameType === 'text') {
       return (
         <div style={style}>
-          <Sandbox level={this.props.match.params.id}/>
-          <Editor level={this.props.match.params.id}/>
+          <div style={shadow}>
+            <Sandbox level={this.props.match.params.id}/>
+            <Editor level={this.props.match.params.id}/>
+          </div>
         </div>
       )
     } else {
