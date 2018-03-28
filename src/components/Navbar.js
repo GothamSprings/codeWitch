05240f3fee31da44e4342931390c603a04b400a8
Roxie Turner <roxie.turner@gmail.com>
2018-03-28 17:12:13 -0400

    userpage styled

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,19 @@ import { firebaseApp } from '../Firebase'
 import * as firebase from 'firebase'
 import '../css/Sign.css'
 
+const GREY = "#9E9E9E";
+
+const buttonStyle = {
+  backgroundColor: 'black',
+  border: '2px solid #ccc',
+  color: `${GREY}`,
+  marginTop: '5px',
+  padding: '15px 32px',
+  textAlign: 'center',
+  textDecoration: 'none',
+  display: 'inline-block',
+  fontSize: '1em'
+}
 
 class Navbar extends Component {
   constructor(props) {
@@ -32,7 +45,7 @@ class Navbar extends Component {
             <li className="left">
               <Link to="/" className="link"> CodeWitch </Link>
             </li>
-            {firebaseApp.auth().currentUser ? <div className="error"><Link to="/userpage"> Welcome {firebaseApp.auth().currentUser.email}</Link><button onClick={this.onClick}>LogOut</button></div> :
+            {firebaseApp.auth().currentUser ? <div className="error"><Link to="/userpage"> Welcome {firebaseApp.auth().currentUser.email}</Link><button style={buttonStyle} className='button' onClick={this.onClick}>LogOut</button></div> :
               <div><li className="right"><Link to="/login" className="link"> Login </Link></li>
               <li className="right"><Link to="/signup" className="link"> SignUp </Link></li></div>}
             </ul>
